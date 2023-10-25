ffcf8013f7b853855711b438a2081f0d662697dc
Chandan Kumar <chandanroy1315@gmail.com>
2023-10-25 15:30:28 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      serverActions: true,
+      serverComponentsExternalPackages: ['mongoose']
+    },
+    images: {
+      domains: ['m.media-amazon.in']
+    }
+  }
+  
+  module.exports = nextConfig
